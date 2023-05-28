b99a466975712b6de3542ab18f368d0d5e57de14
distortedspacetime <97430762+distortedspacetime@users.noreply.github.com>
2023-05-28 21:49:22 +0900

    updated pages

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ const app = http.createServer((request, response) => {
                         </li>
                     </ul>
                 </p>`;
-                var templateHTML = template.HTML(title, description, '');
+                var templateHTML = template.HTML(title, description, '<a href="/create">작성</a>');
                 
             response.writeHead(200);
             response.end(templateHTML);
@@ -55,7 +55,119 @@ const app = http.createServer((request, response) => {
             });
         }
 
-    } else {
+    } else if(pathname === "/create"){
+        var title  = "활동보고서";
+        var html = template.HTML(title, `
+            <form action="http://localhost:3000/create_process" method="POST">
+                <div class="form-group">
+                    <label for="date" style="margin-top: 5px;">날짜</label>
+                    <input type="date" id="date" name="date" required>
+                </div>
+
+                <div class="form-group">
+                    <label for="activity6">활동내역(1)-6교시</label>
+                    <textarea type="text" id="activity6" name="activity6" required></textarea>
+                </div>
+
+                <div class="form-group">
+                    <label for="activity7">활동내역(2)-7교시</label>
+                    </label>
+                    <textarea type="text" id="activity7" name="activity7" required></textarea>
+                </div>
+
+                <div class="form-group">
+                    <label for="name1">김동휘 소감문</label>
+                    <textarea type="text" id="name1" name="name1" required></textarea>
+                </div>
+
+                <div class="form-group">
+                    <label for="name2">김세진 소감문</label>
+                    <textarea type="text" id="name2" name="name2" required></textarea>
+                </div>
+
+                <div class="form-group">
+                    <label for="name3">문태조 소감문</label>
+                    <textarea type="text" id="name3" name="name3" required></textarea>
+                </div>
+
+                <div class="form-group">
+                    <label for="name4">박성완 소감문</label>
+                    <textarea type="text" id="name4" name="name4" required></textarea>
+                </div>
+
+                <div class="form-group">
+                    <label for="name5">박영민 소감문</label>
+                    <textarea type="text" id="name5" name="name5" required></textarea>
+                </div>
+
+                <div class="form-group">
+                    <label for="name6">손기원 소감문</label>
+                    <textarea type="text" id="name6" name="name6" required></textarea>
+                </div>
+
+                <div class="form-group">
+                    <label for="name7">안형준 소감문</label>
+                    <textarea type="text" id="name7" name="name7" required></textarea>
+                </div>
+
+                <div class="form-group">
+                    <label for="name8">허지환 소감문</label>
+                    <textarea type="text" id="name8" name="name8" required></textarea>
+                </div>
+                <div>
+                    <input class="submit_button" type="submit">
+                </div>
+            </form>
+        `, '')
+        response.writeHead(200);
+        response.end(html);
+    } else if(pathname === '/create_process'){
+        var body = "";
+        request.on('data', (data) => {
+            body += data;
+        });
+        request.on("end", () => {
+            const post = qs.parse(body);
+            const date = post.date;
+            const activity6 = post.activity6;
+            const activity7 = post.activity7;
+            const name1 = post.name1;
+            const name2 = post.name2;
+            const name3 = post.name3;
+            const name4 = post.name4;
+            const name5 = post.name5;
+            const name6 = post.name6;
+            const name7 = post.name7;
+            const name8 = post.name8;
+            const description = `
+            <h1>${date}</h1>
+            <p>
+                <h3>활동 내용</h3>
+                <ol>
+                    <li>${activity6}</li>
+                    <li>${activity7}</li>
+                </ol>
+                <h3>소감</h3>
+                <ol>
+                    <li>김동휘: ${name1}</li>
+                    <li>김세진: ${name2}</li>
+                    <li>문태조: ${name3}</li>
+                    <li>박성완: ${name4}</li>
+                    <li>박영민: ${name5}</li>
+                    <li>손기원: ${name6}</li>
+                    <li>안형준: ${name7}</li>
+                    <li>허지환: ${name8}</li>
+                </ol>
+            </p>
+            `
+            fs.writeFile(`data/${date}`, description, 'utf8', (err) => {
+                console.log(description);
+                response.writeHead(302, {location: `/?id=${date}`});
+                response.end();
+            });
+    
+        })
+    }else {
     response.writeHead(404);
     response.end('Not found');
     }
--- template.js
+++ template.js
@@ -13,36 +13,86 @@ module.exports = {
             margin: 0%;
             background: #303030;
             color: white;
+            font-family: "Noto Sans KR", sans-serif;
         }
-        .logo {
-            margin: auto;
+        @font-face {
+            font-family: "Noto Sans KR";
+            font-style: normal;
+            font-weight: 400;
+            font-display: swap;
+            src: url(https://fonts.gstatic.com/s/notosanskr/v13/PbykFmXiEBPT4ITbgNA5Cgm203Tq4JJWq209pU0DPdWuqxJFA4GNDCBYtw.0.woff2)
+                format("woff2");
+        }
+        body {
+            margin: 0%;
+            background: #303030;
+            color: white;
+            font-family: "Noto Sans KR", sans-serif;
+        }
+        label {
             display: block;
-            height: 100px;
+            font-weight: bold;
+            margin-bottom: 5px;
+        }
+        h1 {
+            color: #333;
+            text-align: center;
+            margin-bottom: 10px;
+        }    
+        .logo {
+            display: block;
+            margin: auto;
+        }
+        .logo img{
+            max-width: 42.5%;
         }
         .upper-bar{
-            background: rgba(3, 3, 49, 0.795);
-            padding: 0px auto;
+            background: rgba(0,0,0,0);
+            padding: 10px;
+            text-align: center;
         }
         .body {
-            margin: 20px auto;
-            width: 900px;
+            margin: 0.5% auto;
+            width: 70%;
             background: white;
             color: black;
-            padding: 20px;
-            border-radius: 5px;
+            padding: 1%;
+            border-radius: 20px;
+            box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
         }
         .control {
-            margin: 20px;
+            margin-left: 14%;
+            margin-bottom: 20px;
             width: 100px;
             background: white;
             color: black;
             border-radius: 5px;
             text-align: center;
         }
+        .form-group {
+            margin-bottom: 10px;
+        }
+        textarea,
+        input[type="date"] {
+            width: 100%;
+            padding: 8px;
+            font-size: 16px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            box-sizing: border-box;
+        }
+        .right-align {
+            text-align: right;
+        }
+        .submit_button {
+            margin-bottom: 10px;
+        }
     </style>
     <body>
         <div class="upper-bar">
-            <img class="logo" height="100px" src="https://gcdaeseong-h.gne.go.kr/upload/gcdaeseong-h/clubhmpg_10004924/img_032d66cc-7b10-4896-b8e4-5a6fea10fc711680704204177.png">
+            <div  class="logo">
+                <img src="https://gcdaeseong-h.gne.go.kr/upload/gcdaeseong-h/clubhmpg_10004924/img_032d66cc-7b10-4896-b8e4-5a6fea10fc711680704204177.png" alt="Pythonic Record">
+            </div>
         </div>
         <div class="body">
             ${body}
